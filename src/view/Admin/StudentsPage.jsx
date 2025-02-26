293d3e8df2636bc2b9fa8decce464180fe5a2286
vidusha <dilshanariyarathna1999@gmail.com>
2025-02-27 00:44:30 +0530

    new changes with delete

--- src/view/Admin/StudentsPage.jsx
+++ src/view/Admin/StudentsPage.jsx
@@ -7,10 +7,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Box, IconButton, Switch, Typography } from '@mui/material';
+import { Box, IconButton, Stack, Switch, Typography } from '@mui/material';
 import axios from 'axios';
 import Api from '../../utils/Api';
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Swal from 'sweetalert2';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -32,8 +35,8 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
     },
 }));
 
-function createData(name, batch, classType, email,  regNo, state, id, whatsAppNo) {
-    return { name, batch, classType, email,  regNo, state, id, whatsAppNo };
+function createData(name, batch, classType, email, regNo, state, id, whatsAppNo) {
+    return { name, batch, classType, email, regNo, state, id, whatsAppNo };
 }
 
 
@@ -52,11 +55,11 @@ const StudentsPage = () => {
     console.log(logingId);
 
     const converTrueFales = (state) => {
-        if(state === 'active'){
+        if (state === 'active') {
             return true;
         }
 
-        if(state === 'de-active'){
+        if (state === 'de-active') {
             return false;
         }
     }
@@ -66,15 +69,15 @@ const StudentsPage = () => {
             const response = await axios.get(Api + 'students/student/getallstudents', {
                 headers: {
                     'Authorization': `Bearer ${token}`,
-                    'admin_id':logingId
+                    'admin_id': logingId
                 }
             })
 
             console.log(response)
 
-            if(response.data.code ===200){
+            if (response.data.code === 200) {
                 setStudentDataList(response.data.data);
-                
+
             }
         } catch (err) {
             console.error(err)
@@ -83,8 +86,8 @@ const StudentsPage = () => {
     const rows = studentDataList
         .sort((a, b) => a.batch.localeCompare(b.batch))
         .map(student =>
-        createData(student.name, student.batch, student.classType, student.email,  student.regNo, student.accountState, student._id, student.whatsAppNo)
-    )
+            createData(student.name, student.batch, student.classType, student.email, student.regNo, student.accountState, student._id, student.whatsAppNo)
+        )
 
 
 
@@ -92,14 +95,16 @@ const StudentsPage = () => {
     const updateAccountStatus = async (id, newState) => {
         console.log(id, newState);
         try {
-            const response = await axios.put(`${Api}students/student/state/update/${id}`, 
-                { state: newState }, 
-                { headers: { 
-                    'Authorization': `Bearer ${token}` ,
-                    'admin_id':logingId
-                } }
+            const response = await axios.put(`${Api}students/student/state/update/${id}`,
+                { state: newState },
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'admin_id': logingId
+                    }
+                }
             );
-            
+
             // fetchStudentList();
             setStudentDataList(prevState =>
                 prevState.map(student =>
@@ -116,12 +121,51 @@ const StudentsPage = () => {
         updateAccountStatus(id, newState);
     };
 
-    React.useEffect(() =>{
+    const handleDelete = async(id) => {
+        try{
+            const response = await axios.delete(Api + 'students/student/deletestudent/' + id,{
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+
+            console.log(response);
+
+            if (response.data.code === 200) {
+               await Swal.fire({
+                    title: response.data.message,
+                    icon: "success",
+                    draggable: true,
+                    timer: 3000
+                });
+
+                window.location.reload();
+            }else {
+                Swal.fire({
+                    title: response.data.message,
+                    icon: "warning",
+                    draggable: true,
+                    timer: 3000
+                });
+            }
+
+        } catch (err) {
+            console.error(err);
+            Swal.fire({
+                title: "Something Went Wrong !",
+                icon: "error",
+                draggable: true,
+                timer: 3000
+            });
+        }
+    }
+
+    React.useEffect(() => {
         fetchStudentList();
-    },[])
+    }, [])
 
     return (
-        <Box sx={{minHeight:'75vh'}}>
+        <Box sx={{ minHeight: '75vh' }}>
             <Box>
                 <Typography sx={{
                     fontSize: { xs: '18px', md: '20px' },
@@ -156,14 +200,14 @@ const StudentsPage = () => {
                                     <StyledTableCell align="right">{row.batch}</StyledTableCell>
                                     <StyledTableCell align="right">{row.classType}</StyledTableCell>
                                     <StyledTableCell align="right">
-          {row.email}
-          <IconButton
-            size="small"
-            onClick={() => navigator.clipboard.writeText(row.email)}
-          >
-            <ContentCopyIcon fontSize="small" />
-          </IconButton>
-        </StyledTableCell>
+                                        {row.email}
+                                        <IconButton
+                                            size="small"
+                                            onClick={() => navigator.clipboard.writeText(row.email)}
+                                        >
+                                            <ContentCopyIcon fontSize="small" />
+                                        </IconButton>
+                                    </StyledTableCell>
                                     <StyledTableCell align="right">{row.whatsAppNo}</StyledTableCell>
                                     <StyledTableCell align="right">{row.regNo}</StyledTableCell>
                                     <StyledTableCell align="right">
@@ -172,6 +216,21 @@ const StudentsPage = () => {
                                             onChange={() => handleSwitchChange(row.id, row.state)}
                                             inputProps={{ 'aria-label': 'controlled' }}
                                         />
+                                    </StyledTableCell>
+                                    <StyledTableCell>
+                                        <Stack direction={'row'} spacing={2}>
+                                            <DeleteIcon sx={{
+                                                color:'red',
+                                                ":hover":{
+                                                    scale:1.1
+                                                }
+                                            }} onClick={()=>handleDelete(row.id)}/>
+                                            <EditIcon sx={{
+                                                ":hover":{
+                                                    scale:1.1
+                                                }
+                                            }}/>
+                                        </Stack>
                                     </StyledTableCell>
                                 </StyledTableRow>
                             ))}
